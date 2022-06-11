49da4e9ebe93d176dd84020f9aeec8812bf952d3
Daisaku Yamamoto <yamamoto.daisaku@gmail.com>
2022-06-11 20:12:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Run `npm install` at the root of your directory
+2. Run `npm run start` to start the project
+3. Start coding!
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './styles/App.css';
+import React from "react";
+
+// Constants
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+
+const App = () => {
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {renderNotConnectedContainer()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
